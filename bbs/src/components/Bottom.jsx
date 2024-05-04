0a4952e64ce3ad48a6a1ec4f92cab9adb8d9f3cd
yubin-im <ybyb130@gmail.com>
2024-05-04 12:39:31 +0900

    bbs 프로젝트 코드 이동

---
+++ bbs/src/components/Bottom.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Bottom = () => {
+  return (
+    <div>
+      <hr />
+      <h3>Copyright 2024. 임유빈 All rights reserved.</h3>
+    </div>
+  );
+};
+
+export default Bottom;
